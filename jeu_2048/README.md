46a7d8249a66178b05f79f96387b4adb7831b119
Yassa401 <115942009+Yassa401@users.noreply.github.com>
2022-11-22 08:47:07 +0100

    Delete jeu_2048 directory

--- jeu_2048/README.md
+++
@@ -1,7 +0,0 @@
-# jeu_2048
-
-projet programmation en c pour l'année 2022/2023 En L2 informatique
-
-
-## Compilation :
-commande : make 
--- jeu_2048/projet_alternance/sauvegarde_partie.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef SAUVEGARDE_PARTIE_H
-#define SAUVEGARDE_PARTIE_H
-
-void recup_partie(int n, char * str ,int mat[NB_MAX][NB_MAX],int *score);
-void sauvegarde_score(int meilleur_score);
-void meilleur(int *meilleur_score,int score_actuelle);
-
-#endif
